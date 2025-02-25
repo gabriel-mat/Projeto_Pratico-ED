40e8116b36a08742b02ef2da75074578f53a76fd
gabriel <gabrielmatheusdesouza26@gmail.com>
2025-02-25 15:44:37 -0300

    Correção do menu

--- torre/main.c
+++ torre/main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "../API/fila/fila.h"
 #include "../API/deque/deque.h"
 
 // Protótipos de funções e procedimentos
 
-void imprimir_menu();
+void menu();
+void iniciar();
+void como_jogar();
+void regras();
+
 void limpar_tela();
 
 // == == == == == == == == == == == == == == ==
@@ -14,26 +19,55 @@ int main()
 {
     int codigo;
 
-    // Exibindo o menu
-    imprimir_menu();
+    // Exibindo o menu e recebendo a opção do usuário
+    menu();
 
     return 0;
 }
 
-void imprimir_menu()
+void menu()
 {
     int opt;
 
-    limpar_tela();
+    do
+    {
+        limpar_tela();
 
-    printf("  MENU:\n");
-    printf("\tiniciar\t\t01\n");
-    printf("\tcomo jogar\t02\n");
-    printf("\tregras\t\t03\n");
+        printf("\tMENU:\n");
+        printf("\tiniciar\t\t01\n");
+        printf("\tcomo jogar\t02\n");
+        printf("\tregras\t\t03\n");
 
-    printf("\ninsira o codigo do comando: ");
-    scanf("%d", &opt);
+        printf("\ninsira o codigo do comando: ");
+        scanf("%d", &opt);
+
+        switch (opt)
+        {
+        case 1:
+            iniciar();
+            break;
+
+        case 2:
+            como_jogar();
+            break;
+
+        case 3:
+            regras();
+            break;
+
+        default:
+            opt = 0;
+            printf("Comando invalido! Tente novamente.");
+            scanf("%*c");
+            getchar();
+            break;
+        }
+    } while (!opt);
 }
+
+void iniciar() {}
+void como_jogar() {}
+void regras() {}
 
 void limpar_tela()
 {
