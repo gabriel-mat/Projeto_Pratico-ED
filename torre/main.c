60169b2e6a24851d54d869e52120573c6a820b54
Otávio Inácio de Oliveira <otaviogame25@gmail.com>
2025-02-25 12:28:55 -0300

    botando na main

---
+++ torre/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include"fila.h"
+#include"deque.h"
+
+int traduzircomandomenu(char comando[]) {
+    if (strcmp(comando, "iniciar") == 0) {
+        return 0;
+    } else if (strcmp(comando, "comojogar") == 0) {
+        return 1;
+    } else if (strcmp(comando, "regras") == 0) {
+        return 2;
+    } else {
+        return -1;
+    }
+}
+
+int main() {
+    printf("Digite um comando (iniciar, comojogar, regras): ");
+
+    char comando[10];
+    scanf("%s", comando);
+
+    int codigo = traduzircomandomenu(comando);
+
+    switch (codigo) {
+        case 0:
+            printf("iniciar\n");
+            break;
+        case 1:
+            printf("comojogar\n");
+            break;
+        case 2:
+            printf("regras\n");
+            break;
+        default:
+            printf("COMANDO INVALIDO\n");
+    }
+
+    return 0;
+}
